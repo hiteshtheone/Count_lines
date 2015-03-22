b34040078d54a3077daf21c718d38a816f302c49
hiteshtheone <hiteshtheone@gmail.com>
2015-03-22 20:18:08 +0530

    First Check-in, working

--- Count_lines/main.cpp
+++ Count_lines/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    if (argc != 2) {
+        cout << "Provide text filename!! Closing Program." << endl;
+        return 0;
+    }
+    string data;
+    ifstream inFile;
+    //inFile.open("/Users/hambarkh/Desktop/hitesh/XCode/Count_lines/Count_lines/sample.txt");
+    inFile.open(argv[1]);
+    unsigned int lCount;
+    /*while(!inFile.eof()) {
+        
+        inFile >> data;
+        cout << data << endl;
+        lCount++;
+    }
+     lCount--; //Extra 1 is counted here
+     */
+    char c;
+    while (inFile.get(c)) {
+        if (c == '\n') {
+            lCount++;
+        }
+    }
+    cout << "No. of lines = " << lCount << endl;
     return 0;
 }
